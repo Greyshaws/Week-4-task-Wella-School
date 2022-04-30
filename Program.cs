72da2c33f412ca985f093ba5485b4719937aca70
Gracious Igwe <gracious0901@gmail.com>
2022-04-30 03:55:22 +0100

    Week4Task

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Week4Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a number checking app");
+
+            //To perform a calculation
+            Console.Write("Type in your first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Type in your second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            int result = firstNumber + secondNumber;
+
+            //To make a decision, I used the switch statement
+            switch (result)
+            {
+                case int resultOfCalculation when resultOfCalculation <= 100:
+                Console.WriteLine("True"); //return true
+                break;
+                case int resultOfCalculation when resultOfCalculation >= 100:
+                Console.WriteLine("False"); //return false
+                break;
+                default:
+                Console.WriteLine("Error: Try again");
+                break;
+            }
+        }
+    }
+}
